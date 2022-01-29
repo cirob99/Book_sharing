3f8d7d2284a2e8ee2588364d130bfd40b3351e2a
Ciro <62609536+cirob99@users.noreply.github.com>
2022-01-29 18:53:30 +0100

    funziona schermata iniziale
    ho concluso la parte "logistica" della schermata principale che mostra tutti i libri in possesso di un utente. Prendo lgli id dal database e faccio le chiamate ajax all'api per mostrare le specifiche dei libri. Cosa manca: si deve aggiustare la tabella e si deve fare il collegamento all'id utente.

--- libri.php
+++ libri.php
@@ -1,11 +1,16 @@
 <HTML>
 <HEAD>
-   <TITLE>Anna Goy - Tecnologie Web: approcci avanzati</TITLE>
+   <TITLE>La mia libreria</TITLE>
    <META HTTP-EQUIV="CONTENT-TYPE" CONTENT="TEXT/HTML; CHARSET=utf-8">
-   <LINK REL="STYLESHEET" TYPE="TEXT/CSS" HREF="../anna.css">
+   <link href="style.css" rel="stylesheet" />
    <META HTTP-EQUIV="CACHE-CONTROL" CONTENT="NO-CACHE">
    <script src="jquery-3.4.1.js" type="text/javascript"></script>
-   
+   <!-- bootstrap CSS -->
+   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+</HEAD>
+
+<BODY>
+
 <?php
 	// includo la classe Book
 	include("DataMGR/Book.php"); 
@@ -13,56 +18,92 @@
 	// includo la connessione al database (var $db)
 	require("DataMGR/connDB.php");
 
+	 
 	if ($erroreDB != "") {
 		echo $erroreDB;
 	  }
 	  else {
 		$listaLibri = $db->query("SELECT id_libro FROM utenti_libri JOIN utenti WHERE utenti.id_utente = 2");
-		$libri = array();
-	  foreach ($listaLibri as $l) { // per ogni domanda
-		$id = $l['id_libro'];
-		$libri[] = $id;
-		//echo $id;
-	  }
-	}
+		
+		//if($listaLibri->rowCount()>0) { // se ci sono dei libri...
+			// creo un array
+			$libri = array();
+			
+			//$libri["books"] = array();
+		
+			while ($id_libro = $listaLibri->fetch(PDO::FETCH_ASSOC)){  	
+
+				//$prova= $id_libro['id_libro'];
+
+				$book_item = array(
+					"id" => $id_libro['id_libro'],
+				);
+				
+				//$item= $id_libro['id_libro'];
+			
+			array_push($libri, $book_item);
+
+			//return $json;
+			
+			}
+			$json = json_encode($libri); 
+  
+		}
 ?>
  
- <span id=messaggio> </span>
+ <div id=messaggio> </div>
+
 
  <script>
-   jQuery(document).ready(function() {
-	   console.log('<?= $id ?>');
+ jQuery(document).ready(function() {
+
+	var arr = '<?= $json ?>';
+	const myArr = JSON.parse(arr);
+	console.log(arr);
+	
+	//arr.forEach(item => console.log(item));
+
+	var read_books_html=`
+	<table class='table table-bordered table-hover'>
+	<tr>
+		<th class='w-15-pct'>Titolo</th>
+		<th class='w-10-pct'>Autore</th>
+		<th class='w-15-pct'>Categoria</th>
+		<th class='w-25-pct'>Descrizione</th>
+	</tr>`;
+
+	for (var i = 0; i < myArr.length; i++) {
+		var obj = myArr[i];
+		//$("#messaggio").html("<p>"+arr[i]+"</p><br /><br />");
+    	console.log(obj['id']);  
 
 	$.ajax({
-			url: ('https://www.googleapis.com/books/v1/volumes/'+'<?= $id ?>'),
+			url: ('https://www.googleapis.com/books/v1/volumes/'+ obj['id']),
 			type: "GET",
 		})
 		.done(function (response) {
 			console.log(response);
-			var read_books_html=`
-
-                    <!-- start table -->
-                    <table class='table table-bordered table-hover'>
-                        <tr>
-                            <th class='w-15-pct'>Titolo</th>
-                            <th class='w-10-pct'>Autore</th>
-                            <th class='w-15-pct'>Categoria</th>
-                            <th class='w-25-pct'>Descrizione</th>
-                        </tr>
-                        <tr>
-                                <td>` + response.volumeInfo.title + `</td>
-                                <td>` + response.volumeInfo.authors + ` euro</td>
-                                <td>` + response.volumeInfo.categories + `</td>
-                                <td>` + response.volumeInfo.description + `</td>
-                        </tr>
-                        </table>`
-			$("#messaggio").html("<p>"+read_books_html+"</p><br /><br />");
+			read_books_html= read_books_html+ `
+				<tr>
+						<td>` + response.volumeInfo.title + `</td>
+						<td>` + response.volumeInfo.authors + ` </td>
+						<td>` + response.volumeInfo.categories + `</td>
+						<td>` + response.volumeInfo.description + `</td>
+				</tr>`
+				
+			$("#messaggio").html(read_books_html);
 		})
 		.fail(function (xhr, resp, text) {
 			// stampo l'errore sulla console e sulla pagina (NB sulla pagina, solo in fase di sviluppo!!)
 			console.log(xhr, resp, text);
 			$("messaggio").html(xhr+resp+text);
 		});
+	}
    })
-
+ 
    </script>
+
+</table>
+
+</BODY>
+</HTML>
